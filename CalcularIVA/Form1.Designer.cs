34e42df7732d9f9eb23aebb0698308178d24df7a
Ishwar Lopez Rua <ilopezr101@gmail.com>
2020-08-03 21:28:10 -0500

    programa para calcular IVA de un valor

---
+++ CalcularIVA/Form1.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace CalcularIVA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtingresovalor = new System.Windows.Forms.TextBox();
+            this.btncalcular = new System.Windows.Forms.Button();
+            this.lbliva = new System.Windows.Forms.Label();
+            this.lblvalorfinal = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnclean = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(28, 165);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(294, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ingrese valor del producto";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(315, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(159, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Calcular IVA";
+            // 
+            // txtingresovalor
+            // 
+            this.txtingresovalor.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtingresovalor.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(100)))));
+            this.txtingresovalor.Location = new System.Drawing.Point(344, 164);
+            this.txtingresovalor.Name = "txtingresovalor";
+            this.txtingresovalor.Size = new System.Drawing.Size(241, 32);
+            this.txtingresovalor.TabIndex = 2;
+            this.txtingresovalor.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txtingresovalor.TextChanged += new System.EventHandler(this.txtingresovalor_TextChanged);
+            // 
+            // btncalcular
+            // 
+            this.btncalcular.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btncalcular.Location = new System.Drawing.Point(372, 214);
+            this.btncalcular.Name = "btncalcular";
+            this.btncalcular.Size = new System.Drawing.Size(167, 66);
+            this.btncalcular.TabIndex = 3;
+            this.btncalcular.Text = "Calcular ";
+            this.btncalcular.UseVisualStyleBackColor = true;
+            this.btncalcular.Click += new System.EventHandler(this.btncalcular_Click);
+            // 
+            // lbliva
+            // 
+            this.lbliva.AutoSize = true;
+            this.lbliva.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbliva.Location = new System.Drawing.Point(368, 315);
+            this.lbliva.Name = "lbliva";
+            this.lbliva.Size = new System.Drawing.Size(0, 24);
+            this.lbliva.TabIndex = 4;
+            this.lbliva.Click += new System.EventHandler(this.lbliva_Click);
+            // 
+            // lblvalorfinal
+            // 
+            this.lblvalorfinal.AutoSize = true;
+            this.lblvalorfinal.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblvalorfinal.Location = new System.Drawing.Point(368, 372);
+            this.lblvalorfinal.Name = "lblvalorfinal";
+            this.lblvalorfinal.Size = new System.Drawing.Size(0, 24);
+            this.lblvalorfinal.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label3.Location = new System.Drawing.Point(284, 311);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 28);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "IVA :";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.MediumBlue;
+            this.label4.Location = new System.Drawing.Point(167, 368);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(185, 28);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Valor con IVA :";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // btnclean
+            // 
+            this.btnclean.Font = new System.Drawing.Font("Arial Rounded MT Bold", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnclean.Location = new System.Drawing.Point(610, 166);
+            this.btnclean.Name = "btnclean";
+            this.btnclean.Size = new System.Drawing.Size(102, 32);
+            this.btnclean.TabIndex = 8;
+            this.btnclean.Text = "limpiar";
+            this.btnclean.UseVisualStyleBackColor = true;
+            this.btnclean.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CalcularIVA.Properties.Resources.iva;
+            this.pictureBox1.Location = new System.Drawing.Point(576, 260);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(199, 178);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 9;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnclean);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblvalorfinal);
+            this.Controls.Add(this.lbliva);
+            this.Controls.Add(this.btncalcular);
+            this.Controls.Add(this.txtingresovalor);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "CalcularIVA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtingresovalor;
+        private System.Windows.Forms.Button btncalcular;
+        private System.Windows.Forms.Label lbliva;
+        private System.Windows.Forms.Label lblvalorfinal;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnclean;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ CalcularIVA/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CalcularIVA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void lbliva_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btncalcular_Click(object sender, EventArgs e)
+        {
+            double subtotal = double.Parse(txtingresovalor.Text);
+
+            double calcular = 0;
+
+            double iva = subtotal * 0.19;
+            double total = iva + subtotal;
+
+            lbliva.Text = Convert.ToString(String.Format("{0:n0}", iva));
+            lblvalorfinal.Text = Convert.ToString(String.Format("{0:n0}", total));
+
+
+
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtingresovalor_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            txtingresovalor.Text = String.Empty;
+            lbliva.Text = String.Empty;
+            lblvalorfinal.Text = String.Empty;
+            txtingresovalor.Focus();
+        }
+    }
+}
